aac42e546b0456224e43dcbfa65fd5dbb902d118
Anonymouslybharath <103274684+Anonymouslybharath@users.noreply.github.com>
2023-12-01 17:05:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')  # Replace 'your_template_name.html' with your actual template filename
 
-@app.route('/messenger_integration')
-def messenger_integration():
+@app.route('/Virtual_Assistant')
+def Virtual_Assistant():
     # Replace 'path_to_your_exe.exe' with the actual path to your executable file
     exe_path = 'C:/Users/shash/OneDrive/Desktop/NITK/main.exe'
     
@@ -19,5 +19,21 @@ def messenger_integration():
     
     return redirect(url_for('index'))  # Redirect back to the main page after running the executable
 
+@app.route('/PDF_Chat')
+def PDF_Chat():
+    return redirect('https://gpt.h2o.ai/') # Redirect
+
+@app.route('/Documentation')
+def Documentation():
+    return redirect('https://codellama.h2o.ai/')
+
+@app.route('/messanger')
+def messanger():
+    return redirect('http://127.0.0.1:5000')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
+
+
+
